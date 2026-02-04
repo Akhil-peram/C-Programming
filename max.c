4c5f4f1f0163d7ac1cf0eb3213ee8fa6db0ae475
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-04 23:42:44 +0530

    Add maxnum function to compare two integers

---
+++ max.c
@@ -0,0 +1,15 @@
+/* Functions in C */
+
+
+#include<stdio.h>
+
+void maxnum(int a,int b){
+if (a>b){
+printf("\nA is big: %d",a);}
+else{
+printf("\nB is big:%d",b);
+}
+}
+int main(){
+maxnum(9,6);
+}
